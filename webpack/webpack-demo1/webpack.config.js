83f4bd1cb93f68d7c0c81fdac0baff30b2cedaff
hingbelieve@163.com <Hingbelieve@163.com>
2022-04-12 16:09:08 +0800

    前端工程化学习

--- webpack/webpack-demo1/webpack.config.js
+++ webpack/webpack-demo1/webpack.config.js
@@ -8,7 +8,9 @@ import { __dirname } from './root-dirname.js';
 
 export default {
 	mode: 'development',
-	entry: './src/index.js',
+	entry: {
+		main: './src/index.js'
+	},
 	devtool: 'source-map',
 	plugins: [
 		new CleanWebpackPlugin(),
@@ -19,7 +21,14 @@ export default {
 			content: '竹杖芒鞋轻胜马,谁怕?一蓑烟雨任平生'
 		}),
 		new HtmlWebpackPlugin({
+			filename: 'index.html',
 			template: './public/index.html',
+			inject: 'body',
+			minify: true
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'login.html',
+			template: './public/login.html',
 			inject: 'body',
 			minify: true
 		})
@@ -33,11 +42,23 @@ export default {
 			{
 				test: /\.hq$/i,
 				use: [hqLoader]
+			},
+			{
+				test: /\.(png|jpg|svg|jpeg|gif)$/i,
+				type: 'asset',
+				parser: {
+					dataUrlCondition: {
+						maxSize: 8 * 1024
+					}
+				},
+				generator: {
+					filename: 'assets/img/[name].[hash:6][ext]'
+				}
 			}
 		]
 	},
 	output: {
 		path: path.resolve(__dirname, './dist'),
-		filename: 'bundle.js'
+		filename: 'js/[name].js'
 	}
 };
