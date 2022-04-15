0c5a683aee0d30d91c905936d02db093fdba1362
hingbelieve@163.com <Hingbelieve@163.com>
2022-04-15 16:47:10 +0800

    前端工程化学习

--- webpack/calc.js
+++ webpack/calc.js
@@ -1,18 +1,17 @@
 const arr = [1,5,6,8,3,9,2,0,7,10,11,21,19];
 
 const cache = [];
-for (var i = 0, len = arr.length; i < len; i++) {
+for (let i = 0, len = arr.length; i < len; i++) {
 	const elem = arr[i];
 	const cL = cache.length;
 	if (!cL) {
-		cache.push(elem);
-	} else if (cL === 1 && cache[0] > elem) {
-		cache[0] = elem;
-	} else if (elem < cache[cL - 1] && elem > cache[cL - 2]) {
-		cache[cL - 1] = elem;
-	} else if (elem > cache[cL - 1]) {
-		cache.push(elem);
+		cache.push({ v: elem, i });
+	} else if (cL === 1 && cache[0].v > elem) {
+		cache[0] = { v: elem, i };
+	} else if (elem < cache[cL - 1].v && elem > cache[cL - 2].v) {
+		cache[cL - 1] = { v: elem, i };
+	} else if (elem > cache[cL - 1].v) {
+		cache.push({ v: elem, i });
 	}
 }
-console.log(i);
 console.log(cache);
